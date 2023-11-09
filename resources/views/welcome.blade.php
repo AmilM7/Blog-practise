41a4253e377e912cbd3cee6e09dfa5a8ab7ed3bc
amil_murselovic <amil.murselovic@bicomsystems.com>
2023-11-09 11:38:28 +0100

    - Blade layout basics covered

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,20 @@
-<!DOCTYPE html>
-<link rel="stylesheet" href="/app.css">
+@extends('components/layout')
 
-<title>My Blogs</title>
 
-<body>
-    <?php foreach ($posts as $post) : ?>
-    <div  class="mainDiv">
-        <h1>
-            <a href="post/<?= $post->slug ?>">
-                <?= $post->title; ?>
-            </a>
-        </h1>
-        <article>
-            <?= $post->excerpt; ?>
-        </article>
-    </div>
-    <?php endforeach; ?>    
-</body
+@section('content')
+
+@foreach ($posts as $post)
+<div  class="mainDiv">
+    <h1>
+        <a href="post/<?= $post->slug ?>">
+            {{ $post->title }}
+        </a>
+    </h1>
+    <article>
+       {{ $post->excerpt }}
+    </article>
+</div>
+@endforeach  
+    
+@endsection
+
